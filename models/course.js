f6b7c743194a55bc9c8c757c2000658b8ae6310d
Ahmad-ElHariri <elhariri.ahmad06@gmail.com>
2025-07-06 19:57:47 +0300

    Added course.js

---
+++ models/course.js
@@ -0,0 +1,23 @@
+// Describes a course and its relationship to a creator
+
+const mongoose = require('mongoose');
+
+const courseSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  description: {
+    type: String
+  },
+  creator_id: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  }
+}, {
+  timestamps: true
+});
+
+module.exports = mongoose.model('Course', courseSchema);
