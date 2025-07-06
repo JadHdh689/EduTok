e9d891d6099292de858ab482069f2cac94113d10
Ahmad-ElHariri <elhariri.ahmad06@gmail.com>
2025-07-06 19:58:58 +0300

    Added chapter.js

---
+++ models/chapter.js
@@ -0,0 +1,24 @@
+// Represents chapters within a course, ordered by sequence
+
+const mongoose = require('mongoose');
+
+const chapterSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  sequence_number: {
+    type: Number,
+    required: true
+  },
+  course_id: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Course',
+    required: true
+  }
+}, {
+  timestamps: true
+});
+
+module.exports = mongoose.model('Chapter', chapterSchema);
