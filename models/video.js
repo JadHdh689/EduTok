f4f6ec9477092f099958a5424d38152d8a544f7c
Ahmad-ElHariri <elhariri.ahmad06@gmail.com>
2025-07-06 20:00:42 +0300

    added video.js

---
+++ models/video.js
@@ -0,0 +1,61 @@
+// Stores video details, views, likes, and optional links to course/chapter
+
+const mongoose = require('mongoose');
+
+const videoSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  description: {
+    type: String
+  },
+  url: {
+    type: String,
+    required: true
+  },
+  is_deleted: {
+    type: Boolean,
+    default: false
+  },
+  views_count: {
+    type: Number,
+    default: 0
+  },
+  duration_seconds: {
+    type: Number,
+    max: 90,
+    required: true
+  },
+  category: {
+    type: String
+  },
+  creator_id: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  course_id: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Course',
+    default: null
+  },
+  chapter_id: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Chapter',
+    default: null
+  },
+  likes_count: {
+    type: Number,
+    default: 0
+  },
+  comments_count: {
+    type: Number,
+    default: 0
+  }
+}, {
+  timestamps: true
+});
+
+module.exports = mongoose.model('Video', videoSchema);
