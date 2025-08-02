fd9ecdd24321792092b70b30ddf62465f685ccee
Ahmad-ElHariri <elhariri.ahmad06@gmail.com>
2025-08-02 19:36:33 +0300

    sendemail

---
+++ api/utils/sendEmail.js
@@ -0,0 +1,20 @@
+const nodemailer = require('nodemailer');
+
+const sendEmail = async (to, message) => {
+  const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: process.env.EMAIL_USER,     // your Gmail address
+      pass: process.env.EMAIL_PASS      // app-specific password (not your Gmail password)
+    }
+  });
+
+  await transporter.sendMail({
+    from: `"EduTok" <${process.env.EMAIL_USER}>`,
+    to,
+    subject: 'EduTok Email Verification',
+    text: message
+  });
+};
+
+module.exports = sendEmail;
